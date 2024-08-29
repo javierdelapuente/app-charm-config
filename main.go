2dc89df341c9537085ff7cc2a99f22f8f2c2e5c5
Javier de la Puente <javier.delapuente@canonical.com>
2024-08-29 09:37:07 +0200

    update default flags and doc

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 
 const (
 	defaultCharmcraftLocation = "./"
-	defaultPackageName        = "charmconfig"
-	defaultOutputFile         = "charmconfig.go"
+	defaultPackageName        = "appconfig"
+	defaultOutputFile         = "appconfig.go"
 )
 
 func main() {
 	charmcraftDir := flag.String("c", defaultCharmcraftLocation, "charmcraft.yaml file directory.")
 	packageName := flag.String("p", defaultPackageName, "name of the generated package.")
-	outputFile := flag.String("o", defaultOutputFile, "output file. Overwrites previous file if it exists")
+	outputFile := flag.String("o", defaultOutputFile, "output file. Overwrites the previous file if it exists")
 	flag.Parse()
 
 	err := paascharm.CreateGoStructs(*charmcraftDir, *packageName, *outputFile)
